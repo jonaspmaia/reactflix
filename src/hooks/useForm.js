17cfa7cc8eef2c74d48bb3f465b305c89e56cc9a
Jonas Pires Maia <jonaspiresmaia@gmail.com>
2020-08-04 12:46:40 -0300

    Vegflix

---
+++ src/hooks/useForm.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export default function useForm(valoresIniciais) {
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(key, value) {
+    setValues({
+      ...values,
+      [key]: value,
+    });
+  }
+
+  function handleChange(info) {
+    setValue(
+      info.target.getAttribute('name'),
+      info.target.value,
+    );
+  }
+
+  function clearForm() {
+    setValues(valoresIniciais);
+  }
+
+  return { values, handleChange, clearForm };
+}
--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/button';
+import useForm from '../../../hooks/useForm';
 
 function CadastroCategoria() {
   const valoresIniciais = {
@@ -10,25 +11,14 @@ function CadastroCategoria() {
     cor: '',
   };
 
+  const { handleChange, values, clearForm } = useForm(valoresIniciais);
+
   const [categorias, setCategorias] = useState([]);
-  const [values, setValues] = useState(valoresIniciais);
-
-  function setValue(key, value) {
-    setValues({
-      ...values,
-      [key]: value,
-    });
-  }
-
-  function handleChange(info) {
-    setValue(
-      info.target.getAttribute('name'),
-      info.target.value,
-    );
-  }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://veganflix.herokuapp.com/categorias';
     fetch(URL).then(async (response) => {
       const data = await response.json();
       setCategorias([
@@ -48,7 +38,7 @@ function CadastroCategoria() {
         info.preventDefault();
         setCategorias([...categorias, values]);
 
-        setValues(valoresIniciais);
+        clearForm();
       }}
       >
 
@@ -83,8 +73,8 @@ function CadastroCategoria() {
 
       <ul>
         {categorias.map((categoria) => (
-          <li key={`${categoria.nome}`}>
-            {categoria.nome}
+          <li key={`${categoria.titulo}`}>
+            {categoria.titulo}
           </li>
         ))}
       </ul>
